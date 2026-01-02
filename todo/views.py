ea1cc8b98b7fed95b2349caf61eab438affc4b5e
shashank12-collab <shashanksingh56074@gamil.com>
2026-01-02 18:07:22 +0530

    full functionality

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,9 @@ from todoss.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed = True)
     context= {
         'tasks' : tasks,
+        'completed_task':completed_tasks
     }
     return render(request ,'home.html',context)
